f485b6a902e78ca0ff33913ae46c9ff25a50348d
Ranae Dietzel <ranae.dietzel@gmail.com>
2018-04-25 16:05:33 -0500

    Resampling code from community to check the imbalance of my predictors

--- model_n2o_emissions.R
+++ model_n2o_emissions.R
@@ -48,9 +48,78 @@ R2(plsFit)
 
 #Rank importance of predictor variables 
 #?varImp
-varImp(plsFit)%>%
+varImp(plsFit, ncomp=6)%>%
   rownames_to_column()%>%
   ggplot(aes(x=reorder(rowname, -Overall), y=Overall))+
   geom_bar(stat="identity")+
   coord_flip()+
   labs(y = "Coefficient sum", x = "Predictors")
+
+
+###################################################################
+library(recipes)
+library(rsample)
+library(yardstick)
+
+pls_rec <- recipe(total_N2O ~ ., data = annual_flux) %>%
+  add_role(series, town, avg_N2O, log_avg_N2O,year, log_total_N2O,
+           new_role = "other data") %>%
+  step_YeoJohnson(all_predictors()) %>%
+  step_center(all_predictors()) %>%
+  step_scale(all_predictors())
+
+# functions to fit the model and get predictions + RMSE
+pls_fit <- function(recipe, ...) {
+  plsr(total_N2O ~ ., data = juice(recipe, all_predictors(), all_outcomes()))
+}
+pls_pred <- function(split, recipe, model, ...) {
+  # get the holdout data via `assessment` then preprocess
+  pred_data <- bake(recipe, newdata = assessment(split), all_predictors(), all_outcomes())
+  
+  # One outcome so drop the dimension to a 2d array
+  pred_vals <- predict(model, newdata = pred_data)[,1,] %>% 
+    as.data.frame() %>%
+    bind_cols(assessment(split) %>% select(total_N2O)) %>%
+    # drop the predictions per component into a long df
+    gather(label, prediction, -total_N2O) %>%
+    # convert "X comp" to a numeric X
+    mutate(
+      ncomp = gsub(" comps", "", label),
+      ncomp = as.numeric(ncomp)
+    ) %>%
+    # add resampling labels
+    cbind(labels(split))
+}
+
+
+# setup a resampling scheme, estimate the recipes, fit the models and 
+# make predictions, 
+set.seed(57256)
+folds <- vfold_cv(annual_flux, repeats = 5) %>%
+  mutate(
+    recipes = map(splits, prepper, pls_rec, retain = TRUE),
+    models = map(recipes, pls_fit, validation = "none"),
+    pred = pmap(
+      list(split = splits, recipe = recipes, model = models),
+      pls_pred
+    )
+  )
+
+# get the RMSE values, summarize by #comps and plot
+
+pred_vals <- folds %>% 
+  pull(pred) %>% 
+  bind_rows()
+
+performance <- pred_vals %>%
+  group_by(ncomp, id, id2) %>%
+  do(metrics(., truth = total_N2O, estimate = prediction))
+
+av_performance <- performance %>% 
+  group_by(ncomp) %>%
+  summarize(rmse = mean(rmse), rsq = mean(rsq))
+
+ggplot(av_performance, aes(x = ncomp, y = rsq)) + 
+  geom_point() + 
+  geom_line() + 
+  theme_bw()
